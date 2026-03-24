7c58f18baa38fe7d8f92cfe3512a9c89f7915d2b
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-24 09:18:57 -0400

    added basic mongo connection

---
+++ app.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config(); // need this for .env 
+}
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+const db_url = process.env.DB_URL || "mongodb://127.0.0.1:27017/dbss"
+
+mongoose.connect(db_url);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database Connected!");
+});
+
+app.listen(3000, () => {
+  console.log('App is listening!')
+})
