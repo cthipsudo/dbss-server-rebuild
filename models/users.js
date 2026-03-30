b711ee3ea38be8ad28511816e1d0825fdff20e95
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-30 17:04:43 -0400

    updated users model

---
+++ models/users.js
@@ -0,0 +1,23 @@
+// CREATE TABLE dbss_users (
+//   id SERIAL PRIMARY KEY,
+//   user_name TEXT NOT NULL,
+//   password TEXT NOT NULL,
+//   date_created TIMESTAMPTZ NOT NULL DEFAULT now(),
+//   date_modified TIMESTAMPTZ
+// );
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema(
+  {
+    id: String,
+    user_name: String,
+    password: String,
+    date_created: Date,
+    date_modifed: Date
+  }
+)
+
+module.exports = mongoose.model('User', UserSchema);
