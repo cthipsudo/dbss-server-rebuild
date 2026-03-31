c4440e368cf7f9647e182af5a1dc98c9727bbc0f
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-03-31 17:53:07 -0400

    connected choice routes

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
+const path = require("path");
+const engine = require("ejs-mate");
+const apiRoutes = require("./routes/api");
 
 const mongoose = require("mongoose");
-
 const db_url = process.env.DB_URL || "mongodb://127.0.0.1:27017/dbss";
-
 mongoose.connect(db_url);
 
 const db = mongoose.connection;
@@ -16,6 +17,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database Connected!");
 });
+
+app.engine("ejs", engine);
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+app.use("/", apiRoutes);
 
 app.listen(3000, () => {
   console.log("App is listening!");
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/choices").get();
+router.route("/choices").get((req, res) => {
+  console.log("I work!");
+  res.send("Hi!");
+});
+
+module.exports = router;
