52c64a888f84d4be6d7931974dae4325bd644e35
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-04-01 17:54:44 -0400

    removed sanitized

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const helmet = require("helmet");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 
-const mongoSanitize = require("express-mongo-sanitize");
 const choicesRoute = require("./routes/choices");
 const apiRoute = require("./routes/api");
 const questionsRoute = require("./routes/questions");
@@ -31,7 +30,6 @@ app.engine("ejs", engine);
 app.use(helmet());
 app.use(cors({ origin: "https://dbss-client.vercel.app" }));
 app.use(express.json({ limit: "10kb" }));
-app.use(mongoSanitize());
 
 app.use("/api", rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use("/api", apiRoute);
